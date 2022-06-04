b0a1adce60afd25344ee90bf3ae478190534f8e5
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-04 06:54:34 +0530

    [Zeeshan] : Added Logic for preliminary mood analysis

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome To Mood Analyzer Problem!!");
+            Console.WriteLine("\t\t\t\t    ----------------------------------------");
+            string message = "I am very sad";
+            MoodAnalyserLogic moodAnalyser = new MoodAnalyserLogic(message);
+            System.Console.WriteLine("I am in a {0} mood. ",moodAnalyser.AnalyseMood());
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MoodAnalyser
+# Mood Analyser Problem
+
+[UC1] : Given a Message, ability to analyse and respond Happy or Sad Mood
